c6062729892a7b197e9d75db10eda4af4126230c
TannabiZz <120448549+CannabiZz9@users.noreply.github.com>
2023-03-07 13:41:29 +0700

    finish

--- average.cpp
+++ average.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    int sum=0, count=0;
+    
+    if(argc !=1 ){
+       
+        for (int i = 1; i < argc; i++) {
+    sum += atof(argv[i]);
+    count++;
+    }
+    sum = sum/count;
+    cout << "---------------------------------\n";
+    cout << "Average of " << count << " numbers = " << sum << "\n";
+    cout << "---------------------------------\n";
+    }
+    else{
+     cout << "Please input numbers to find average.";
+    }
+
+    return 0;
+}
